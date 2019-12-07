11df6669c18807e1bd7ee3446585ea18d5dc6b0b
alexmocanu07 <46425351+alexmocanu07@users.noreply.github.com>
2019-12-07 16:52:14 +0200

    Dashboard
    Dashboard init

---
+++ amz-frontend/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Sal</p>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
