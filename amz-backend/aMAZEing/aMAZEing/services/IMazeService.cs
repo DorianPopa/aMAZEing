455ede57718e8cf6f26a7173c16306873333ae6d
XPS-9560\Dorian <popa.dorian98@gmail.com>
2019-12-30 12:51:36 +0200

    Added MazeService interface

---
+++ amz-backend/aMAZEing/aMAZEing/services/IMazeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using aMAZEing.DTOs;
+using aMAZEing.models;
+using aMAZEing.utils;
+
+namespace aMAZEing.services
+{
+    public interface IMazeService
+    {
+        Maze CreateMaze(Maze maze);
+        MazeDTO CreateMazeByUserId(Guid userId, MazeFE maze);
+        List<MazeDTO> GetAllMazes();
+        MazeDTO GetMazeById(Guid id);
+        MazeVisualizerDTO Visualize(MazeFE maze, string algorithm);
+    }
+}
