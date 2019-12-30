2738f698e785f88ecb17714e59e7bde7d76c8af0
XPS-9560\Dorian <popa.dorian98@gmail.com>
2019-12-30 12:50:00 +0200

    Removed ValidateMaze method from IAlgorithmService

--- amz-backend/aMAZEing/aMAZEing/services/IAlgorithmService.cs
+++ amz-backend/aMAZEing/aMAZEing/services/IAlgorithmService.cs
@@ -1,13 +1,10 @@
 ﻿using aMAZEing.utils;
-using System;
 using aMAZEing.DTOs;
 
 namespace aMAZEing.services
 {
     public interface IAlgorithmService
     {
-        String ValidateMaze(MazeFE mazeFE);
-
         MazeVisualizerDTO Visualize(MazeFE mazeFE);
     }
 }
