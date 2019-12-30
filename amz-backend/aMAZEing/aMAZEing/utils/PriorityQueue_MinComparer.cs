48342df7863de2381fbf5564c111a0f6883bc24a
XPS-9560\Dorian <popa.dorian98@gmail.com>
2019-12-30 12:46:56 +0200

    Priority queue implemented

---
+++ amz-backend/aMAZEing/aMAZEing/utils/PriorityQueue_MinComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace aMAZEing.utils
+{
+    public class PriorityQueue_MinComparer : IComparer<double>
+    {
+        public int Compare(double x, double y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+}
