f6def1ef3acb3da137cc6b3719a270bbeb8eabbd
XPS-9560\Dorian <popa.dorian98@gmail.com>
2019-12-30 12:47:33 +0200

    Point class changed to a struct

--- amz-backend/aMAZEing/aMAZEing/utils/Point.cs
+++ amz-backend/aMAZEing/aMAZEing/utils/Point.cs
@@ -1,23 +1,29 @@
-﻿namespace aMAZEing.utils
+﻿using System;
+
+namespace aMAZEing.utils
 {
-    public class Point
+    public struct Point
     {
         public int I { get; set; }
-
         public int J { get; set; }
-
         public int Value { get; set; }
 
-        public Point()
-        {
+        // public Point previous ;
 
-        }
-
-        public Point(int next_i, int next_j, int v)
+        public Point(int next_i, int next_j, int v):this()
         {
             this.I = next_i;
             this.J = next_j;
             this.Value = v;
         }
+        public double DistanceTo(Point p2)
+        {
+            return Math.Sqrt(
+                Math.Pow((this.I - p2.I), 2) + 
+                Math.Pow((this.J - p2.J), 2)
+                );
+        }
+
+        public static Point Default { get { return new Point(); } }
     }
 }
